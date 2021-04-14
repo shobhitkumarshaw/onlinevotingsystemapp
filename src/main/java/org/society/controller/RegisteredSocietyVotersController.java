c18bbd845104f7f436db2273a1abe5366011de6e
AdityaMohapatra038 <adityamohapatra107@gmail.com>
2021-04-14 23:28:17 +0530

    Update RegisteredSocietyVotersController.java

--- src/main/java/org/society/controller/RegisteredSocietyVotersController.java
+++ src/main/java/org/society/controller/RegisteredSocietyVotersController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/RegisteredSocietyVoter")
+@RequestMapping("/api/RegisteredSocietyVoter")
 public class RegisteredSocietyVotersController {
 
 	Logger logger = LoggerFactory.getLogger(RegisteredSocietyVotersController.class);
