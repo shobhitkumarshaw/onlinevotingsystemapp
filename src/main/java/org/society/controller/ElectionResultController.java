b1943f17748f1887a033a26c9ec515d0e1a90c91
RitikKumarKumar <81379844+RitikKumarKumar@users.noreply.github.com>
2021-04-14 20:20:10 +0530

    logger

--- src/main/java/org/society/controller/ElectionResultController.java
+++ src/main/java/org/society/controller/ElectionResultController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.society.entities.ElectionResult;
 import org.society.exceptions.ElectionResultNotFoundException;
 import org.society.exceptions.EmptyDataException;
@@ -27,14 +29,17 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class ElectionResultController {
 	@Autowired
 	private ElectionResultService service;
+	Logger logger = LoggerFactory.getLogger(ElectionResultController.class);
 
 	
 	@GetMapping
 	public List<ElectionResult> getAllResult() {
 		List<ElectionResult> list = service.viewElectionResultList();
 		if (list.size() == 0) {
+			logger.error("No data found in Election Result database!");
 			throw new EmptyDataException("No result in database!");
 		}
+		logger.info("Election Result list found!");
 		return list;
 	}
 
@@ -42,15 +47,18 @@ public class ElectionResultController {
 	@GetMapping(value = "{CandidateId}")
 	public ResponseEntity<?> getResult(@PathVariable("CandidateId") long candidateId) {
 		ElectionResult er = service.viewCandidatewiseResult(candidateId);
-		if (er == null)
+		if (er == null) {
+			logger.error("No data found in Election Result database!");
 			throw new ElectionResultNotFoundException("Election Result not found!");
-
+		}
+		logger.info("Admin id: " + candidateId + " found!");
 		return new ResponseEntity<ElectionResult>(er, HttpStatus.OK);
 	}
 
 	@PostMapping
 	public String saveResult(@Valid @RequestBody ElectionResult result) {
 		service.addElectionResult(result);
+		logger.info("Election Result added with id: " + result.getId());
 		return "Election Result successfully saved";
 	}
 
@@ -58,6 +66,7 @@ public class ElectionResultController {
 	public String updateResult(@Valid @RequestBody ElectionResult result) {
 
 		service.updateElectionResult(result);
+		logger.info("Elction Result with id: " + result.getId() + " updated!");
 		return "Election Result successfully Updated";
 	}
 
@@ -65,6 +74,7 @@ public class ElectionResultController {
 	public String deleteResult(@PathVariable("CandidateId") long CandidateId) {
 
 		service.deleteElectionResult(CandidateId);
+		logger.info("Election Result with id: " + CandidateId + " deleted!");
 		return "Election Result data successfully deleted";
 
 	}
