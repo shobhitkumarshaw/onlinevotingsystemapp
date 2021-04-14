6aff130c7e7f1494a39eb8ad086985aedb6ea14e
AdityaMohapatra038 <adityamohapatra107@gmail.com>
2021-04-14 20:51:57 +0530

    Data added

--- src/main/java/org/society/OnlinevotingsystemappApplication.java
+++ src/main/java/org/society/OnlinevotingsystemappApplication.java
@@ -3,8 +3,6 @@ package org.society;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
