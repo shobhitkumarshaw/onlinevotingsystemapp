fc34ecb90bcabe8eeb20fc678db4df0ac3f9a5aa
Govind1121 <k.govind4@gmail.com>
2021-05-05 16:23:42 +0530

    cross Origin nominated

--- src/main/java/org/society/controller/NominatedCandidatesController.java
+++ src/main/java/org/society/controller/NominatedCandidatesController.java
@@ -17,6 +17,7 @@ import org.society.service.NominatedCandidatesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/NominatedCandidates")
 public class NominatedCandidatesController {
 
