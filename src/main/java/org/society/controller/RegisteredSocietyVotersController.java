096104d54e9d1c9456ac93e4f161e71314e6878a
AdityaMohapatra038 <adityamohapatra107@gmail.com>
2021-05-06 01:20:42 +0530

    Small changes

--- src/main/java/org/society/controller/RegisteredSocietyVotersController.java
+++ src/main/java/org/society/controller/RegisteredSocietyVotersController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/RegisteredSocietyVoter")
