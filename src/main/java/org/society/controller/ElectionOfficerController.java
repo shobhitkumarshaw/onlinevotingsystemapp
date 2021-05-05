f8234f16855f31b04cbb4d88d6b1a0e3e64039d5
shobhitkumarshaw <56765072+shobhitkumarshaw@users.noreply.github.com>
2021-05-05 16:10:15 +0530

    commit

--- src/main/java/org/society/controller/ElectionOfficerController.java
+++ src/main/java/org/society/controller/ElectionOfficerController.java
@@ -12,6 +12,7 @@ import org.society.service.ElectionOfficerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/ElectionOfficer")
 public class ElectionOfficerController {
 	
