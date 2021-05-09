8518e4fdb9961d8f59894d748447bc924293a8eb
RitikKumarKumar <81379844+RitikKumarKumar@users.noreply.github.com>
2021-05-09 21:23:59 +0530

    reg data

--- src/main/java/org/society/DBinitRegisteredSocietyVoters.java
+++ src/main/java/org/society/DBinitRegisteredSocietyVoters.java
@@ -17,8 +17,8 @@ public class DBinitRegisteredSocietyVoters implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-	registeredSocietyVotersRepository.save(new RegisteredSocietyVoters(100L,"v12345", "Kishore", "Pal", "Male", "st", "5549315672", "kishore.pal@outlook.com", "Japjpur", "Jappur Road", "Bhubhanswer", 634896, true, "active", null));
-	registeredSocietyVotersRepository.save(new RegisteredSocietyVoters(101L,"v22345", "Ashish", "Singh", "Male", "sc", "8521674392", "ashish.singh@yahoo.com", "Rayagada", "Kotepada", "Gunupur", 573942, true, "active", null));
-	registeredSocietyVotersRepository.save(new RegisteredSocietyVoters(102L,"v32345", "Rasmita", "Panda","Female", "gen", "5463128795", "rasmita.panda@yahoo.com", "Berhampur", "Brahama Nagar", "Berhampur", 521463, false, "active", null));
+	registeredSocietyVotersRepository.save(new RegisteredSocietyVoters(100L,"v400", "Kishore", "Pal", "Male", "st", "5549315672", "kishore.pal@outlook.com", "Japjpur", "Jappur Road", "Bhubhanswer", 634896, true, "active", null));
+	registeredSocietyVotersRepository.save(new RegisteredSocietyVoters(101L,"v401", "Ashish", "Singh", "Male", "sc", "8521674392", "ashish.singh@yahoo.com", "Rayagada", "Kotepada", "Gunupur", 573942, true, "active", null));
+	registeredSocietyVotersRepository.save(new RegisteredSocietyVoters(102L,"v402", "Rasmita", "Panda","Female", "gen", "5463128795", "rasmita.panda@yahoo.com", "Berhampur", "Brahama Nagar", "Berhampur", 521463, false, "active", null));
 	}
 }
